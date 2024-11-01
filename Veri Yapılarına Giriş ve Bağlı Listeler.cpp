fd5a7790c9488bc3f0365051609feca16083fa8a
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-11-01 13:53:35 +0300

    Ders 1

---
+++ Veri Yapılarına Giriş ve Bağlı Listeler.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+struct n {
+	int x;
+	n* next;
+};
+
+typedef n node;
+
+int main()
+{
+	node* root;
+	root = (node*)malloc(sizeof(node));
+	root->x = 10;
+	root->next = (node*)malloc(sizeof(node));
+	root->next->x = 20;
+	root->next->next = (node*)malloc(sizeof(node));
+	root->next->next->x = 30;
+
+	node* iter;
+	iter = root;
+	cout << iter << "\n";
+	cout << iter->x << "\n";
+	iter = iter->next;
+	cout << iter << "\n";
+	cout << iter->x << "\n";
+	iter = iter->next;
+	cout << iter << "\n";
+	cout << iter->x << "\n";
+	return 0;
+}
