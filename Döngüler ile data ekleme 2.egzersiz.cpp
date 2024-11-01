fd70b8b37544cf99a204276aa6e8374e06b24f90
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-11-01 16:12:19 +0300

    video 2

---
+++ Döngüler ile data ekleme 2.egzersiz.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct n {
+	int x;
+	n* next;
+};
+typedef n node;
+int main() 
+{
+	node* root;
+	root = (node*)malloc(sizeof(node));
+	root->x = 10;
+	root->next = (node*)malloc(sizeof(node));
+	root->next->x = 10;
+	root->next->next = (node*)malloc(sizeof(node));
+	root->next->next->x = 30;
+	root->next->next->next = (node*)malloc(sizeof(node));
+	root->next->next->next->x = 40;
+	root->next->next->next->next = (node*)malloc(sizeof(node));
+	root->next->next->next->next->x = 50;
+	root->next->next->next->next->next = NULL;
+	node* iter;
+	iter = root;
+	while (iter != NULL) 
+	{
+		cout << "Adress: " << iter << " Value: " << iter->x << "\n";
+		iter = iter->next;
+	}
+	iter = root;
+	while (iter->next != NULL)
+	{
+		iter = iter->next;
+	}
+	for (int i = 0; i <= 5; i++) 
+	{
+		iter = iter->next;
+		iter = (node*)malloc(sizeof(node));
+		iter->x = (i + 1) * 2;
+		cout << "Adress: " << iter << " Value: " << iter->x << "\n";
+
+	}
+	return 0;
+}
