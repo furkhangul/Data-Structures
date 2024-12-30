a0ed98c63fb67c7f57a1589fde5e8e03dd96d0ca
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-12-30 17:32:55 +0300

    .

---
+++ Final/13.Radix Sort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	//Radix Sort, aynı basamak değerinin bireysel rakamlarını önce gruplayarak öğeleri sıralayan bir sıralama algoritmasıdır .
+	// Daha sonra öğeleri artan/azalan sıralarına göre sıralar.
+	//sayıları basamaklarının üzerinde işlem yaparak sıralayan doğrusal sıralama algoritmasıdır.
+
+	//Örnek soru:
+	//Dizi: [57, 43, 24, 213, 44, 102, 70, 37, 111, 23] dizisini Radix sort'a göre sıralayınız.
+	//1.adım: [70, 111, 102, 43, 213, 23, 24, 44, 57, 37] şeklinde birler basamağı sıralanır.
+	//2.adım: [102, 111, 213, 23, 24, 37, 43, 44, 57, 70] şeklinde onlar basamağı sıralanır.
+	//3.adım: [23, 24, 37, 43, 44, 57, 70, 102, 111, 213] şeklinde yüzler basamağı sıralanır.
+
+	return 0;
+}
